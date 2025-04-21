6cde19d314bda7e42452356638da9f862a1bb36e
Anup Gupta <anup.gupta@nonstopio.com>
2025-04-21 16:45:33 +0530

    Initialize Drools POC project with Maven setup, including wrapper scripts, configuration files, and basic application structure.

---
+++ src/main/java/com/poc/droolspocv2/DroolsPocV2Application.java
@@ -0,0 +1,13 @@
+package com.poc.droolspocv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsPocV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsPocV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/droolspocv2/DroolsPocV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.droolspocv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsPocV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
